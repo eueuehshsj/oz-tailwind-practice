03437ebdd1c21a6fd4ab39dbd3035ce2fdf4a6f2
이용기 <yg99040177@gmail.com>
2026-02-19 13:24:12 +0900

    과제수행

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,14 @@ import Header from "./components/Header";
 function App() {
   return (
     <main>
-      <Header />
+      <Header classname="flex flex-wrap justify-between gap-20 pt-20 pb-20 pr-40 pl-40" />
       <section>
         {contents.map((el) => (
-          <Content key={el.id} content={el} />
+          <Content
+            classname="flex flex-col items-start gap-5"
+            key={el.id}
+            content={el}
+          />
         ))}
       </section>
     </main>
